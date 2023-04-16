a6f5fac91fa7977781907e0b9acf14e302461eef
laflame102 <laflame4308@gmail.com>
2023-04-16 21:20:30 +0300

    did some fixes

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,12 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
+import { selectIsLoggedIn, selectIsRefreshing } from 'redux/auth/selectors';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  // const isRefreshing = useSelector(sele)
+  const isRefreshing = useSelector(selectIsRefreshing);
 
-  const shouldRedirect = !isLoggedIn;
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,12 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
+import { selectUserEmail } from 'redux/auth/selectors';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
+  const email = useSelector(selectUserEmail);
 
   return (
     <div>
-      <p>Welcome, userName</p>
+      <p>Welcome, {email}</p>
       <button type="button" onClick={() => dispatch(logOut())}>
         Log out
       </button>
